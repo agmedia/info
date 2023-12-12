7d8e51a3c69c97df8577cf859c73cf9349ebf1b2
tomashina <tomislav@agmedia.hr>
2023-12-12 10:11:43 +0100

    mail template fix

--- resources/views/emails/contact-form.blade.php
+++ resources/views/emails/contact-form.blade.php
@@ -4,16 +4,16 @@
     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
         <tr>
             <td style="padding: 20px 20px 10px 20px; font-family: sans-serif; font-size: 18px; font-weight: bold; line-height: 20px; color: #555555; text-align: center;">
-                Poruka s Web kontakt forme.<br>
+                Contact Form Message<br>
             </td>
         </tr>
         <tr>
             <td style="padding: 20px 20px 0 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
-                Dobili ste poruku s Web kontakt forme.<br>
+                Contact Form Message<br>
                 <br>
                 <table cellspacing="0" cellpadding="0" border="0" width="100%">
                     <tr>
-                        <td style="width: 26%">Ime:</td>
+                        <td style="width: 26%">Name:</td>
                         <td style="width: 74%"><b>{{ $contact['template-contactform-name'] }}</b></td>
                     </tr>
                     <tr>
@@ -22,8 +22,22 @@
                     </tr>
                     @if ( ! empty($contact['template-contactform-phone']))
                         <tr>
-                            <td>Telefon:</td>
+                            <td>Phone:</td>
                             <td><b>{{ $contact['template-contactform-phone'] }}</b></td>
+                        </tr>
+                    @endif
+
+                    @if ( ! empty($contact['company']))
+                        <tr>
+                            <td>Company:</td>
+                            <td><b>{{ $contact['company'] }}</b></td>
+                        </tr>
+                    @endif
+
+                    @if ( ! empty($contact['template-contactform-subject']))
+                        <tr>
+                            <td>Subject:</td>
+                            <td><b>{{ $contact['template-contactform-subject'] }}</b></td>
                         </tr>
                     @endif
                 </table>
@@ -36,8 +50,8 @@
         </tr>
         <tr>
             <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; text-align: center;">
-                <a href="{{ route('index') }}" style="display: block; display: inline-block; width: 200px; min-height: 20px; padding: 10px; background-color: #a50000; border-radius: 3px; color: #ffffff; font-size: 15px; line-height: 25px; text-align: center; text-decoration: none; -webkit-text-size-adjust: none;">
-                    Idi na stranicu
+                <a href="{{ route('index') }}" style="display: block; display: inline-block; width: 200px; min-height: 20px; padding: 10px; background-color:#1A3956; border-radius: 3px; color: #ffffff; font-size: 15px; line-height: 25px; text-align: center; text-decoration: none; -webkit-text-size-adjust: none;">
+                    Go to web
                 </a>
             </td>
         </tr>
